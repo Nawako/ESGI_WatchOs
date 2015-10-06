50c85120822b240fd13ef66be36c14d99ede7233
Etudiant <Etudiant@iMac-de-Etudiant-23.local>
2015-10-06 12:02:25 +0200

    Ajout d'une nouvelle vu et de la classe lié à la vue

---
+++ TestESGIWatch WatchKit Extension/DetailInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  DetailInterfaceController.h
+//  TestESGIWatch
+//
+//  Created by Etudiant on 06/10/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface DetailInterfaceController : WKInterfaceController
+
+@end
---
+++ TestESGIWatch WatchKit Extension/DetailInterfaceController.m
@@ -0,0 +1,39 @@
+//
+//  DetailInterfaceController.m
+//  TestESGIWatch
+//
+//  Created by Etudiant on 06/10/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "DetailInterfaceController.h"
+
+@interface DetailInterfaceController ()
+
+@end
+
+@implementation DetailInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+- (IBAction)touchDetailButton {
+    NSLog(@"Detail OK");
+}
+
+@end
+
+
+
